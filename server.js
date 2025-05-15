c8747de2b10fb0a05fa76f9b0a328799fef932d4
anuchaj <anuchaj50@gmail.com>
2025-05-15 17:56:20 +0100

    packages and server

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
